0f6ad47cf8c090527a3fcf8090a85b720d0fa514
Dan Haywood <dan@haywood-associates.co.uk>
2015-04-17 00:43:50 +0100

    Further improvements to conversion script for AssertJ

--- convert-junit-assertions-to-assertj.sh
+++ convert-junit-assertions-to-assertj.sh
@@ -34,15 +34,18 @@ echo ''
 FILES=`find . -name "$FILES_PATTERN"`
 
 for EXPRESSION in \
-'s/assertThat(\(.*\),.*is(not(nullValue())))/assertThat(\1).isNotNullValue()/g' \
-'s/assertThat(\(.*\),.*is(nullValue()))/assertThat(\1).isNullValue()/g' \
+'s/assertThat(\(.*\),.*is(not(nullValue())))/assertThat(\1).isNotNull()/g' \
+'s/assertThat(\(.*\),.*is(notNullValue()))/assertThat(\1).isNotNull()/g' \
+'s/assertThat(\(.*\),.*is(nullValue()))/assertThat(\1).isNull()/g' \
 's/assertThat(\(.*\),.*is(false))/assertThat(\1).isFalse()/g' \
 's/assertThat(\(.*\),.*is(true))/assertThat(\1).isTrue()/g' \
 's/assertThat(\(.*\).size(),.*is(\(.*\)))/assertThat(\1).hasSize(\2)/g' \
-'s/assertThat(\(.*\),.*is(not(\(.*\))))/assertThat(\1).isNotEqualTo(\2)/g' \
-'s/assertThat(\(.*\),.*is(\(.*\)))/assertThat(\1).isEqualTo(\2)/g' \
 's/assertThat(\(.*\),.*is(not(equalTo(\(.*\)))))/assertThat(\1).isNotEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(not(greaterThan(\(.*\)))))/assertThat(\1).isLessThanOrEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(not(\(.*\))))/assertThat(\1).isNotEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(greaterThan(\(.*\))))/assertThat(\1).isGreaterThan(\2)/g' \
 's/assertThat(\(.*\),.*is(equalTo(\(.*\))))/assertThat(\1).isEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(\(.*\)))/assertThat(\1).isEqualTo(\2)/g' \
 's/assertThat(\(.*\),.*not(containsString(\(.*\))))/assertThat(\1).doesNotContain(\2)/g' \
 's/assertThat(\(.*\),.*containsString(\(.*\)))/assertThat(\1).contains(\2)/g' \
 's/org\.junit\.Assert\.assertThat\;/org.assertj.core.api.Assertions.assertThat\;/g'
