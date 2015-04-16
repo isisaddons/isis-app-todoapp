4cc46feeab9efa3904ca7e0ce9fcb831b2a1539e
Dan Haywood <dan@haywood-associates.co.uk>
2015-04-17 00:28:06 +0100

    Updating convert script for some hamcrest assertions.

--- convert-junit-assertions-to-assertj.sh
+++ convert-junit-assertions-to-assertj.sh
@@ -34,38 +34,27 @@ echo ''
 FILES=`find . -name "$FILES_PATTERN"`
 
 for EXPRESSION in \
-'s/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g' \
-'s/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g' \
-'s/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g' \
-'s/assertEquals([[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\1).isEmpty()/g' \
-'s/assertEquals(\(\".*\"\),[[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size())/assertThat(\3).as(\1).hasSize(\2)/g' \
-'s/assertEquals([[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size())/assertThat(\2).hasSize(\1)/g' \
-'s/assertEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isCloseTo(\2, within(\4))/g' \
-'s/assertEquals([[:blank:]]*\([^"]*\),[[:blank:]]*\([^"]*\),[[:blank:]]*\([^"]*\))/assertThat(\2).isCloseTo(\1, within(\3))/g' \
-'s/assertEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isEqualTo(\2)/g' \
-'s/assertEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isEqualTo(\1)/g' \
-'s/assertArrayEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isEqualTo(\2)/g' \
-'s/assertArrayEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isEqualTo(\1)/g' \
-'s/assertNull(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isNull()/g' \
-'s/assertNull([[:blank:]]*\(.*\))/assertThat(\1).isNull()/g' \
-'s/assertNotNull(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isNotNull()/g' \
-'s/assertNotNull([[:blank:]]*\(.*\))/assertThat(\1).isNotNull()/g' \
-'s/assertTrue(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isTrue()/g' \
-'s/assertTrue([[:blank:]]*\(.*\))/assertThat(\1).isTrue()/g' \
-'s/assertFalse(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isFalse()/g' \
-'s/assertFalse([[:blank:]]*\(.*\))/assertThat(\1).isFalse()/g' \
-'s/assertSame(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isSameAs(\2)/g' \
-'s/assertSame([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isSameAs(\1)/g' \
-'s/assertNotSame(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isNotSameAs(\2)/g' \
-'s/assertNotSame([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isNotSameAs(\1)/g' \
-'s/import static org.junit.Assert.assertEquals;/import static org.assertj.core.api.Assertions.assertThat;/g' \
-'s/import static org.junit.Assert.fail;/import static org.assertj.core.api.Assertions.fail;/g' \
-'s/import static org.junit.Assert.\*;/import static org.assertj.core.api.Assertions.*;/g'
+'s/assertThat(\(.*\),.*is(not(nullValue())))/assertThat(\1).isNotNullValue()/g' \
+'s/assertThat(\(.*\),.*is(nullValue()))/assertThat(\1).isNullValue()/g' \
+'s/assertThat(\(.*\),.*is(false))/assertThat(\1).isFalse()/g' \
+'s/assertThat(\(.*\),.*is(true))/assertThat(\1).isTrue()/g' \
+'s/assertThat(\(.*\).size(),.*is(\(.*\)))/assertThat(\1).hasSize(\2)/g' \
+'s/assertThat(\(.*\),.*is(not(\(.*\))))/assertThat(\1).isNotEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(\(.*\)))/assertThat(\1).isEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(not(equalTo(\(.*\)))))/assertThat(\1).isNotEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*is(equalTo(\(.*\))))/assertThat(\1).isEqualTo(\2)/g' \
+'s/assertThat(\(.*\),.*not(containsString(\(.*\))))/assertThat(\1).doesNotContain(\2)/g' \
+'s/assertThat(\(.*\),.*containsString(\(.*\)))/assertThat(\1).contains(\2)/g' \
+'s/org\.junit\.Assert\.assertThat\;/org.assertj.core.api.Assertions.assertThat\;/g'
 do
+    echo ""
+    echo ""
+    echo ""
     echo "$EXPRESSION"
     for FILE in $FILES
     do
-        sed $EXPRESSION $FILE > /tmp/$$ 2>/dev/null
+        echo "  $FILE"
+        sed $EXPRESSION $FILE > /tmp/$$ 
         if [ $? -eq 0 ]
         then
             chmod 777 $FILE
@@ -78,7 +67,33 @@ do
 done
 
 echo ''
-echo 'Now:'
-echo '- optimize imports with your IDE to remove unused imports'
-echo '- add "import static org.assertj.core.api.Assertions.within;" if you were using JUnit number assertions with delta'
+echo 'Now optimize imports with your IDE to remove unused imports'
 
+
+# 's/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g' \
+# 's/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g' \
+# 's/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g' \
+# 's/assertEquals([[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\1).isEmpty()/g' \
+# 's/assertEquals(\(\".*\"\),[[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size())/assertThat(\3).as(\1).hasSize(\2)/g' \
+# 's/assertEquals([[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size())/assertThat(\2).hasSize(\1)/g' \
+# 's/assertEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isCloseTo(\2, within(\4))/g' \
+# 's/assertEquals([[:blank:]]*\([^"]*\),[[:blank:]]*\([^"]*\),[[:blank:]]*\([^"]*\))/assertThat(\2).isCloseTo(\1, within(\3))/g' \
+# 's/assertEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isEqualTo(\2)/g' \
+# 's/assertEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isEqualTo(\1)/g' \
+# 's/assertArrayEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isEqualTo(\2)/g' \
+# 's/assertArrayEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isEqualTo(\1)/g' \
+# 's/assertNull(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isNull()/g' \
+# 's/assertNull([[:blank:]]*\(.*\))/assertThat(\1).isNull()/g' \
+# 's/assertNotNull(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isNotNull()/g' \
+# 's/assertNotNull([[:blank:]]*\(.*\))/assertThat(\1).isNotNull()/g' \
+# 's/assertTrue(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isTrue()/g' \
+# 's/assertTrue([[:blank:]]*\(.*\))/assertThat(\1).isTrue()/g' \
+# 's/assertFalse(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isFalse()/g' \
+# 's/assertFalse([[:blank:]]*\(.*\))/assertThat(\1).isFalse()/g' \
+# 's/assertSame(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isSameAs(\2)/g' \
+# 's/assertSame([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isSameAs(\1)/g' \
+# 's/assertNotSame(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isNotSameAs(\2)/g' \
+# 's/assertNotSame([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isNotSameAs(\1)/g' \
+# 's/org\.junit\.Assert.assertEquals;/org.assertj.core.api.Assertions.assertThat;/g' \
+# 's/org\.junit\.Assert.fail;/org.assertj.core.api.Assertions.fail;/g' \
+# 's/org\.junit\.Assert.\*;/org.assertj.core.api.Assertions.*;/g'
