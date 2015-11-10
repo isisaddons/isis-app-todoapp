913eb8270968d5e292e1f616c4dfa597426db5f1
Dan Haywood <dan@haywood-associates.co.uk>
2015-11-10 23:57:00 +0000

    adding bumpver_isis.sh script

---
+++ bumpver_isis.sh
@@ -0,0 +1,15 @@
+VERSION=$1
+
+if [ ! "$VERSION" ]; then
+    echo "usage: $(basename $0) [version]"
+    exit 1
+fi
+
+# edit parent pom.xml
+echo "editing parent pom.xml"
+cat pom.xml | sed "s/<isis.version>.*</<isis.version>$VERSION</" > pom.xml.$$.sed
+mv pom.xml.$$.sed pom.xml
+
+echo "Committing changes"
+git commit -am "bumping isis.version to $VERSION"
+
